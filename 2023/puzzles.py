a8e8bd84d1f4fe09ac50d81e1c8e37cdeb7f4eb7
Alexander Olferuk <olferukab@gmail.com>
2023-12-03 11:01:07 +0200

    2023, 1-2

--- 2023/puzzles.py
+++ 2023/puzzles.py
@@ -2,6 +2,18 @@ import requests
 import numpy as np
 
 
+def download_puzzle(day: int, year: int = 2015) -> str:
+    headers = {
+        "cookie": (
+            'session=53616c7465645f5f59ee1a2b43d34f4327c8c09d04478ed9e8acd1782d780899f0d695b78b0f53d303783ae43df6384c'
+        )
+    }
+    response = requests.get(
+        f"https://adventofcode.com/{year}/day/{day}/input", headers=headers
+    )
+    return response.text
+
+
 def load(day: int) -> str:
     with open(f'data/{day:02d}.txt') as f:
         data = f.read()
